0c680b45318f338e09d37b5565d390416c8fb132
Andrey Kumykov <aakumykov@yandex.ru>
2015-02-20 09:57:24 +0700

    500x400

---
+++ Assets/ListTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ListTest : MonoBehaviour {
+	
+	void Awake () 
+	{
+		Debug.Log ("=========================================");
+		Debug.Log ("screen.size: "+Screen.width+"x"+Screen.height);
+		Debug.Log ("=========================================");
+		Debug.Log ("transform.position: " + transform.position);
+		Debug.Log ("transform.localPosition: " + transform.localPosition);
+
+		Rect rect = gameObject.GetComponent<RectTransform> ().rect;
+		Debug.Log ("rect.position: " + rect.position);
+		Debug.Log ("rect.size: " + rect.size);
+		Debug.Log ("=========================================");
+	}
+
+}
